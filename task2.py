9b5d377dccaf1d6c3fd63d2ef4b6d403d5438f0c
pochuu <92790567+pochuu@users.noreply.github.com>
2021-10-25 01:26:38 +0200

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,28 +1,30 @@
 from itertools import combinations, product
 
-numbers = {
-    '1': '',
-    '2': 'abc',
-    '3': 'def',
-    '4': 'ghi',
-    '5': 'jkl',
-    '6': 'mno',
-    '7': 'pqrs',
-    '8': 'tuv',
-    '9': 'wxyz'
-}
 
+class Text_combination:
+    numbers = {
+        '1': '',
+        '2': 'abc',
+        '3': 'def',
+        '4': 'ghi',
+        '5': 'jkl',
+        '6': 'mno',
+        '7': 'pqrs',
+        '8': 'tuv',
+        '9': 'wxyz'
+    }
 
-def generate_letters(number):
-    if number == "":
-        return []
-    arrays = []
-    result = []
-    for char in number:
-        arrays.append(numbers[char])
-    arrays = product(*arrays)
-    for array in arrays:
-        result.append(''.join(list(array)))
-    return result
+    def generate_letters(self, number):
+        if number == "":
+            return []
+        arrays = []
+        result = []
+        for char in number:
+            arrays.append(self.numbers[char])
+        arrays = product(*arrays)
+        for array in arrays:
+            result.append(''.join(list(array)))
+        return result
 
-print(generate_letters("46"))
+task2 = Text_combination()
+print(task2.generate_letters("46"))
