9175129b4e0d029ebe9847f2537d21936be3e95a
pochuu <92790567+pochuu@users.noreply.github.com>
2021-10-25 01:26:49 +0200

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,15 +1,21 @@
-def reverse_digits(n):
-    if abs(n) >= 0xffffffff:
-        return 0
-    if n >= 0:
-        n = str(n)
-        return n[::-1]
-    else:
-        n = str(n)
-        n = n[1:]
-        n = '-' + n[::-1]
-        return n
+class Reverse_numbers:
+    def __init__(self):
+        self.n = 0
+
+    def reverse_digits(self, n):
+        self.n = n
+        if abs(self.n) >= 0xffffffff:
+            return 0
+        if self.n >= 0:
+            self.n = str(n)
+            return n[::-1]
+        else:
+            self.n = str(self.n)
+            self.n = self.n[1:]
+            self.n = '-' + self.n[::-1]
+            return int(self.n)
 
 
-result = reverse_digits(-1243)
-print(result)
+result = Reverse_numbers()
+result.reverse_digits(-1243)
+print(result.n)
